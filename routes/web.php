bb5197bc40809d28660800ad5c857ad4a12f994b
Sani Asani <saniii@Sanis-MacBook-Pro.local>
2023-11-28 12:27:39 +0100

    fixes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use \App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test', function () {
+    return Response('<h1>Testttt</h1>',200);
+});
+Route::get('/listings', function () {
+    return view('listings',['title'=>"This is a test"]);
+});
+
+Route::get('/users/{id?}',function (?int $id = 1){
+
+    $users = \Illuminate\Support\Facades\DB::table('users')->get();
+    $user = \Illuminate\Support\Facades\DB::table('users')->get()->where('id','==',$id)->first();
+    return view('users', ['users'=>$users, 'sani'=>$user]);
+});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
